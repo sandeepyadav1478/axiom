33ed66bffa7ca3f6beacea910748b07ffd7bc266
Sandeep Yadav <sandeepyadav1478@gmail.com>
2025-11-21 11:02:01 +0530

    Fix validation_history column names to match actual schema

--- axiom/pipelines/airflow/dags/data_quality_validation_dag.py
+++ axiom/pipelines/airflow/dags/data_quality_validation_dag.py
@@ -113,7 +113,7 @@ def validate_last_5_minutes(**context):
     cur.execute("""
         INSERT INTO validation_history (
             validation_run_time, records_checked, records_passed, records_failed,
-            window_start, window_end
+            validation_period_start, validation_period_end
         ) VALUES (%s, %s, %s, %s, %s, %s)
     """, (current_time, record_count, passed, failed, window_start, current_time))
     
